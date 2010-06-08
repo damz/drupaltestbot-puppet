b3dd033238c4d4821438048b47549bcae203921b
Damien Tournoud <damien@commerceguys.com>
2010-06-09 01:21:20 +0200

    Change the naming convention of test clients to allow more flexible hostnames.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,25 +1,21 @@
-
 import "base"
 
-node "qa-3.damz.org" {
+# If the name of the server contains -mysql, use the MySQL profile.
+node /-mysql/ {
   include base
   include testing_bot
   include testing_bot::mysql
 }
 
-node /\.mysql\.drupal-testing\.local$/ {
-  include base
-  include testing_bot
-  include testing_bot::mysql
-}
-
-node /\.pgsql\.drupal-testing\.local$/ {
+# If the name of the server contains -pgsql, use the PostgreSQL profile.
+node /-pgsql/ {
   include base
   include testing_bot
   include testing_bot::pgsql
 }
 
-node /\.sqlite3\.drupal-testing\.local$/ {
+# If the name of the server contains -pgsql, use the SQLite profile.
+node /-sqlite3/ {
   include base
   include testing_bot
   include testing_bot::sqlite3
