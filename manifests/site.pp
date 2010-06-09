60abe74f3106eedf52ef66db62a96ceb889d4ebe
Damien Tournoud <damien@commerceguys.com>
2010-06-10 01:52:08 +0200

    The node regexp needs to be able to generate a valid tag name.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,21 @@
 import "base"
 
 # If the name of the server contains -mysql, use the MySQL profile.
-node /-mysql/ {
+node /mysql/ {
   include base
   include testing_bot
   include testing_bot::mysql
 }
 
 # If the name of the server contains -pgsql, use the PostgreSQL profile.
-node /-pgsql/ {
+node /pgsql/ {
   include base
   include testing_bot
   include testing_bot::pgsql
 }
 
 # If the name of the server contains -pgsql, use the SQLite profile.
-node /-sqlite3/ {
+node /sqlite3/ {
   include base
   include testing_bot
   include testing_bot::sqlite3
